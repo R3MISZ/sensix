8c6ce554ce6dd5a2c170789f09399b6e82fc78a1
Kacper <remiszkacp@gmail.com>
2025-11-27 11:13:51 +0100

    create(DTO): Data Transfer Object for Measurements

---
+++ src/backend/Sensix.Api/Dtos/MeasurementDtos.cs
@@ -0,0 +1,20 @@
+﻿namespace Sensix.Api.Dtos;
+
+// Response obj
+public record MeasurementDto(
+    Guid Id,
+    double Value,
+    DateTime TimestampUtc,
+    string Unit);
+
+// for POST /api/measurements
+public record CreateMeasurementDto(
+    double Value,
+    DateTime? TimestampUtc,
+    string Unit);
+
+// for PUT /api/measurements/{id}
+public record UpdateMeasurementDto(
+    double? Value,
+    DateTime? TimestampUtc,
+    string? Unit);
