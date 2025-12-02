5896d46acbbda6913b3e81d83b3e8a1e572202fd
Kacper <remiszkacp@gmail.com>
2025-12-02 01:47:21 +0100

    update(Database): migration autogenerated

--- src/backend/Sensix.Infrastructure/Migrations/20251201225137_InitialCreate.Designer.cs
+++ src/backend/Sensix.Infrastructure/Migrations/20251201225137_InitialCreate.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Sensix.Infrastructure;
@@ -11,9 +12,11 @@ using Sensix.Infrastructure;
 namespace Sensix.Infrastructure.Migrations
 {
     [DbContext(typeof(SensixDbContext))]
-    partial class SensixDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251201225137_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
