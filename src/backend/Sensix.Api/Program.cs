f5a43c84596b7ec3acfa0f668cef419b04fcdafa
Kacper <remiszkacp@gmail.com>
2025-12-17 16:12:45 +0100

    add(Cors): frontend access

--- src/backend/Sensix.Api/Program.cs
+++ src/backend/Sensix.Api/Program.cs
@@ -6,6 +6,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+        policy.WithOrigins("http://localhost:5174")
+              .AllowAnyHeader()
+              .AllowAnyMethod());
+});
+
 // Add Lib
 builder.Services.AddLibServices();
 
@@ -22,6 +30,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
+app.UseCors();
+
 app.MapControllers();
 
 app.Run();
