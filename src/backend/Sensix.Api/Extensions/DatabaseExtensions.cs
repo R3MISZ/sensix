fb4fd3fdd107e9e971cfa8af7eeda1890db6f1d4
Kacper <remiszkacp@gmail.com>
2026-02-12 13:46:54 +0100

    split(builder service)

---
+++ src/backend/Sensix.Api/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sensix.Lib.Database;
+
+namespace Sensix.Api.Extensions;
+
+public static class DatabaseExtensions
+{
+    public static void AddDatabaseContext(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<SensixDbContext>(options => options.UseNpgsql(connectionString));
+    }
+
+    public static async Task ApplyMigrationsAsync(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+
+        var context = scope.ServiceProvider.GetRequiredService<SensixDbContext>();
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+        try
+        {
+            if ((await context.Database.GetPendingMigrationsAsync()).Any())
+            {
+                await context.Database.MigrateAsync();
+                logger.LogInformation("Database migrated successfully");
+            }
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An error occurred during migration");
+        }
+    }
+}
---
+++ src/backend/Sensix.Api/Extensions/ServiceExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Sensix.Lib.Repository;
+using Sensix.Lib.Service;
+
+namespace Sensix.Api.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void AddApplicationServices(this IServiceCollection services)
+    {
+        // Business Logic
+        services.AddScoped<IDeviceService, DeviceService>();
+        services.AddScoped<ISensorService, SensorService>();
+        services.AddScoped<IMeasurementService, MeasurementService>();
+
+        // Repositories
+        services.AddScoped<IDbRepository, DbRepository>();
+        services.AddScoped<ISensorRepository, SensorRepository>();
+        services.AddScoped<IDeviceRepository, DeviceRepository>();
+        services.AddScoped<IMeasurementRepository, MeasurementRepository>();
+    }
+
+    public static void AddCustomCors(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(policy =>
+                policy.WithOrigins("http://localhost:5173", "http://localhost:5174")
+                      .AllowAnyHeader()
+                      .AllowAnyMethod());
+        });
+    }
+}
