76ad7a3a882be7c41ced1ffda1a4c1a6d525cef6
Kacper <remiszkacp@gmail.com>
2026-02-25 09:42:07 +0100

    update(mock data)

--- src/frontend/src/assets/data_mock.ts
+++ src/frontend/src/assets/data_mock.ts
@@ -10,36 +10,27 @@ const generateMockData = (
   return Array.from({ length: count }).map((_, i) => ({
     id: `mea_${sensorId}_${i}`,
     sensorId: sensorId,
-    timestampUtc: now - (count - i) * 600_000, 
+    timestampUtc: new Date(now - (count - i) * 600_000).toISOString(),
     value: Number((min + Math.random() * (max - min)).toFixed(1))
   }));
 };
 
 export const mock_devices: Device[] = [
-    { createdAtUtc: Date.now(), id: "dev_a", name: "Device A", isActive: true, location: "Lab-a"},
-    { createdAtUtc: Date.now(), id: "dev_b", name: "Device B", isActive: true, location: "Lab-b" },
-    { createdAtUtc: Date.now(), id: "dev_c", name: "Device C", isActive: false, location: "Lab-c"},
-    { createdAtUtc: Date.now(), id: "dev_d", name: "Device D", isActive: false, location: "Lab-d"},
-  ]
+  { createdAtUtc: new Date().toISOString(), id: crypto.randomUUID(), name: "Device A", isActive: true, location: "Lab-A"},
+  { createdAtUtc: new Date().toISOString(), id: crypto.randomUUID(), name: "Device B", isActive: true, location: "Lab-B" },
+  { createdAtUtc: new Date().toISOString(), id: crypto.randomUUID(), name: "Device C", isActive: false, location: "Lab-C"},
+  { createdAtUtc: new Date().toISOString(), id: crypto.randomUUID(), name: "Device D", isActive: false, location: "Lab-D"},
+]
 
 export const mock_sensors: Sensor[] = [
-    { createdAtUtc: Date.now(), id: "sen_a", deviceId: "dev_a", name: "Sensor A", type: "Temperature",  unit: "°C", isActive: true},
-    { createdAtUtc: Date.now(), id: "sen_x", deviceId: "dev_a", name: "Sensor X", type: "Humidity",  unit: "%", isActive: true},
-    { createdAtUtc: Date.now(), id: "sen_b", deviceId: "dev_b", name: "Sensor B", type: "Humidity",  unit: "%", isActive: true},
-    { createdAtUtc: Date.now(), id: "sen_c", deviceId: "dev_c", name: "Sensor C", type: "Pressure",  unit: "bar", isActive: false},
-  ]
+  { createdAtUtc: new Date().toISOString(), id: crypto.randomUUID(), deviceId: mock_devices[0].id, name: "Sensor A", type: "Temperature",  unit: "°C", isActive: true},
+  { createdAtUtc: new Date().toISOString(), id: crypto.randomUUID(), deviceId: mock_devices[0].id, name: "Sensor X", type: "Humidity",  unit: "%", isActive: true},
+  { createdAtUtc: new Date().toISOString(), id: crypto.randomUUID(), deviceId: mock_devices[1].id, name: "Sensor B", type: "Humidity",  unit: "%", isActive: true},
+  { createdAtUtc: new Date().toISOString(), id: crypto.randomUUID(), deviceId: mock_devices[2].id, name: "Sensor C", type: "Pressure",  unit: "bar", isActive: false},
+]
 
 export const mock_measurements: Measurement[] = [
-    ...generateMockData("sen_a", 20, 0, 28),
-    ...generateMockData("sen_b", 20, 0, 100),
-    
-    /*
-    { id: "mea_1", sensor_id: "sen_a", timeStamp: Date.now() - 300_000, value: 21},
-    { id: "mea_2", sensor_id: "sen_a", timeStamp: Date.now() - 150_000, value: 15},
-    { id: "mea_3", sensor_id: "sen_a", timeStamp: Date.now(), value: 5},
-
-    { id: "mea_4", sensor_id: "sen_b", timeStamp: Date.now(), value: 22},
-    { id: "mea_5", sensor_id: "sen_c", timeStamp: Date.now(), value: 23},
-     */
-  ]
-
+  ...generateMockData(mock_sensors[0].id, 20, 0, 28),
+  ...generateMockData(mock_sensors[1].id, 20, 0, 100),
+  ...generateMockData(mock_sensors[2].id, 20, 0, 100),
+]
